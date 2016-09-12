1f75fc0dccc4ed54db042fc5dad6e416cac25be2
daniel_l <gotaotao123@163.com>
2016-09-12 11:42:56 +0800

    adf

--- Application/Home/Common/function.php
+++ Application/Home/Common/function.php
@@ -144,14 +144,14 @@
         $upload_path2 = './Uploads/file/';
         dir_exisit($upload_path2);
         $upload = new \Think\Upload();// 实例化上传类
-        $upload->maxSize   =     3145728 ;// 设置附件上传大小
-        $upload->exts      =     array('xls','doc','rar','docx','jpg','png','gif','xlsx','pdf');// 设置附件上传类型
+        $upload->maxSize   =     31457280 ;// 设置附件上传大小
+        $upload->exts      =     array('xls','doc','rar','docx','jpg','png','gif','xlsx','pdf','PDF');// 设置附件上传类型
         $upload->autoSub   =     false; //关闭子目录
         $upload->rootPath  =     $upload_path2; // 设置附件上传根目录
         // 上传单个文件
         $info   =   $upload->uploadOne($_FILES[$name]);
         if(!$info) {// 上传错误提示错误信息
-            $this->error($upload->getError());
+            exit($upload->getError());
 
         }else{// 上传成功 获取上传文件信息
             return $upload_path.$info['savepath'].$info['savename'];

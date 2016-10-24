23fdebacfe4daeb171f5eefcaf14cf69154b107f
daniel_l <gotaotao123@163.com>
2016-10-24 16:13:08 +0800

    asdf

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -505,7 +505,7 @@ class IndexController extends Controller {
             $upload_path2 = './Uploads/';
             dir_exisit($upload_path2);
             $upload = new \Think\Upload();// 实例化上传类
-            $upload->maxSize   =     3145728 ;// 设置附件上传大小
+            $upload->maxSize   =     31457280 ;// 设置附件上传大小
             $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
             $upload->rootPath  =     $upload_path2; // 设置附件上传根目录
             $upload->savePath  =     ''; // 设置附件上传（子）目录
